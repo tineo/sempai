a019f952851b5ea58d9c892c36d541271db462f3
Cesar Gutierrez Tineo <a20062161@pucp.edu.pe>
2022-09-09 01:11:31 -0500

    initial tucambista

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 sempai
+
+
+create table tucambista
+(
+	id int auto_increment,
+	bidRate DECIMAL(6,4) default 0 not null,
+	offerRate DECIMAL(6,4) default 0 not null,
+	bidReferenceRate DECIMAL(6,4) default 0 not null,
+	offerReferenceRate DECIMAL(6,4) default 0 not null,
+	dateRate DATE NULL,
+	timeRate TIME NULL,
+	datetimeRate DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
+	timestampRate TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+	constraint tucambista_pk
+		primary key (id)
+);
+
---
+++ index.php
@@ -0,0 +1,67 @@
+<?php
+require 'vendor/autoload.php';
+date_default_timezone_set('America/Lima');
+
+use GuzzleHttp\Client;
+
+$client = new Client([
+    'base_uri' => 'https://apim.tucambista.pe',
+    'timeout'  => 2.0,
+]);
+
+
+$res = $client->request('POST', '/api/transaction/quote?', [
+    'headers'        => [
+        'Content-Type' => 'application/json',
+        'Ocp-Apim-Subscription-Key' => 'e4b6947d96a940e7bb8b39f462bcc56d;product=tucambista-production'
+    ],
+    'json' => [
+        
+            "amount"  => 1000,
+            "buyOrSell" => "SELL",
+            "ccy" => "PEN",
+            "totalCreditsToUse" => 0,
+            "cancelPromotionCode" => "",
+            "promotionCode" => ""
+        
+    ]
+]);
+
+$data = json_decode($res->getBody());
+
+$config = parse_ini_file("config.ini", true);
+
+$host = $config["database"]["host"];
+$db   = $config["database"]["db"];
+$user = $config["database"]["user"];
+$pass = $config["database"]["pass"];
+$port = "3306";
+$charset = 'utf8';
+
+$options = [
+    \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+    \PDO::ATTR_EMULATE_PREPARES   => false,
+];
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset;port=$port";
+try {
+     $pdo = new \PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
+
+$dateRate =  date("Y-m-d");
+$timeRate = date("H:i:d");
+
+
+
+$sql = "INSERT INTO tucambista (bidRate, offerRate, bidReferenceRate, offerReferenceRate, dateRate, timeRate) VALUES (?,?,?,?,?,?)";
+$stmt= $pdo->prepare($sql);
+$stmt->execute([
+    $data->bidRate, 
+    $data->offerRate, 
+    $data->bidReferenceRate, 
+    $data->offerReferenceRate, 
+    $dateRate, 
+    $timeRate
+    ]);
